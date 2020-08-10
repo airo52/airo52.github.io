41bc6f8d5e23500727e542b4e7123a3a5b9f4699
airo52 <54649171+airo52@users.noreply.github.com>
2020-08-10 16:27:13 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 window.onload = () => {
   setTimeout(() => {
     document.querySelector("body").classList.add("display");
+    $('#serviceBox').load('services.html')
   }, 2000);
 };
 
@@ -20,6 +21,10 @@ document.querySelector(".home").addEventListener("click", () => {
   document.querySelector(".container").classList.toggle("change");
 });
 
+document.querySelector(".Service").addEventListener("click", () => {
+  document.querySelector(".container").classList.toggle("change");
+ 
+});
 document.querySelector(".about").addEventListener("click", () => {
   document.querySelector(".container").classList.toggle("change");
 });
@@ -30,3 +35,51 @@ document.querySelector(".scroll-btn").addEventListener("click", () => {
     document.querySelector("html").style.scrollBehavior = "unset";
   }, 1000);
 });
+
+$('#submit').click(function(){
+  var fullName = $('#name').val();
+  var tel = $('#tel').val();
+  var email = $('#email').val();
+  var message = $('#message').val();
+  //alert(message)
+  if(fullName !=='' && tel !=='' && email !=='' && message !==''){
+    var telphone =tel.replace(/\D/g,'');
+    if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
+      $control ='contactss';
+      var data = 'email='+email+'&fullName='+fullName+'&control='+$control+'&telephone='+telphone+'&message='+message;
+          
+        $.ajax({
+          
+            method:'POST',
+            data:data,
+            url:'admin/server/index.php',
+            cache:false,
+            success:function(res){
+              $('#name').val('');
+                $('#tel').val('');
+                $('#email').val('');
+                 $('#message').val('');
+              $('#details').html(res);
+    setTimeout(() => {
+      $('#details').html('');
+    }, 2000);
+    
+            }
+        })
+
+    }else{
+      $('#details').html('wrong email format');
+    setTimeout(() => {
+      $('#details').html('');
+    }, 2000);
+    }
+
+  }else{
+    $('#details').html('all fileds are required');
+    setTimeout(() => {
+      $('#details').html('');
+    }, 2000);
+  }
+
+})
+
